b3fa59bae3b40a56eb9b3cf00f0efd3b024c151a
vicke18 <viktor.larsgarde.java25@edu.edugrade.se>
2026-05-04 21:39:43 +0200

    add project structure

---
+++ backend/voyagr/src/test/java/com/example/voyagr/VoyagrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voyagr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyagrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
